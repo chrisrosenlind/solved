ca48ac4ef9f8614f5fa8b291226c058a9f4d9504
Christoffer <christoffer.rosenlind@gmail.com>
2026-04-25 10:23:21 +0200

    Change hand type

--- src/cards/hand.rs
+++ src/cards/hand.rs
@@ -1,14 +1,13 @@
 use crate::cards::Card;
 
 #[derive(Debug)]
-pub struct Hand([Card; 2]);
+pub struct Hand {
+    pub card1: Card,
+    pub card2: Card,
+}
 
 impl Hand {
     pub fn new(card1: Card, card2: Card) -> Self {
-        Hand([card1, card2])
-    }
-
-    pub fn cards(&self) -> &[Card; 2] {
-        &self.0
+        Hand { card1, card2 }
     }
 }
