9cdb4168a8ceea42f3f2a6187c6fbc7d951836bd
Christoffer <christoffer.rosenlind@gmail.com>
2026-04-29 19:20:29 +0200

    Use repr

--- src/cards/card.rs
+++ src/cards/card.rs
@@ -7,20 +7,21 @@ pub enum Suit {
 }
 
 #[derive(Debug)]
+#[repr(u8)]
 pub enum Rank {
-    Two = 0,
-    Three = 1,
-    Four = 2,
-    Five = 3,
-    Six = 4,
-    Seven = 5,
-    Eight = 6,
-    Nine = 7,
-    Ten = 8,
-    Jack = 9,
-    Queen = 10,
-    King = 11,
-    Ace = 12,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
 }
 
 impl From<u8> for Rank {
